dd3370c85cf65f888e0a03202a01bc5d31d4d193
Rodrigo Cevas <rcevas@gmail.com>
2019-12-12 14:26:17 +0100

    commit

--- rodtel/src/app/app-routing.module.ts
+++ rodtel/src/app/app-routing.module.ts
@@ -1,8 +1,41 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccommodationComponent } from './accommodation/accommodation.component';
+import { AuthComponent } from './auth/auth.component';
+import { CelebrationsComponent } from './celebrations/celebrations.component';
+import { FoodBeverageComponent } from './food-beverage/food-beverage.component';
+import { LifestyleComponent } from './lifestyle/lifestyle.component';
+import { OffersComponent } from './offers/offers.component';
+import { WeddingsComponent } from './weddings/weddings.component';
+import { RoomsComponent } from './accommodation/rooms/rooms.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { BarComponent } from './food-beverage/bar/bar.component';
+import { RestaurantsComponent } from './food-beverage/restaurants/restaurants.component';
+import { ExcursionsComponent } from './lifestyle/excursions/excursions.component';
+import { ExperiencesComponent } from './lifestyle/experiences/experiences.component';
+import { ResortActivitiesComponent } from './lifestyle/resort-activities/resort-activities.component';
+import { WellnessAndSpaComponent } from './lifestyle/wellness-and-spa/wellness-and-spa.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'accommodation', component: AccommodationComponent},
+  {path: 'auth', component: AuthComponent},
+  {path: 'celebrations', component: CelebrationsComponent},
+  {path: 'food-beverage', component: FoodBeverageComponent},
+  {path: 'lifestyle', component: LifestyleComponent},
+  {path: 'offers', component: OffersComponent},
+  {path: 'weddings', component: WeddingsComponent},
+  {path: 'rooms', component: RoomsComponent},
+  {path: 'signin', component: SigninComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'bar', component: BarComponent},
+  {path: 'restaurants', component: RestaurantsComponent},
+  {path: 'excursions', component: ExcursionsComponent},
+  {path: 'experiences', component: ExperiencesComponent},
+  {path: 'resort-activities', component: ResortActivitiesComponent},
+  {path: 'wellness-and-spa', component: WellnessAndSpaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
